67b17d197beb36aac9801d0a5479c52ebdd8ee93
Kush <kokatekush@gmail.com>
2024-03-14 22:08:43 +0530

    Initial commit

---
+++ ui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+THEME_COLOR = "#375362"
+
+class QuizzApp:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quiz")
+
+
+        self.window.mainloop()
+
+
